8fe6161ce85ccc46320dff29ea29794a8f52f09c
Derek Stavis <dekestavis@gmail.com>
2016-10-03 18:22:53 -0300

    second implementation without classes

--- test.js
+++ test.js
@@ -1,8 +1,30 @@
 const pagarme = require('./index.js')
 
-const login = new pagarme.strategy.LoginPassword('user', 'pass')
+const emailPasswordAuth = { 
+  email: 'jose@silva.com',
+  password: '12345xuxa',
+  onExpire: function () {
+	console.log('session expired')
+  }
+}
 
-pagarme.connect({ email: 'jose@silva.com', password: '12345xuxa', onExpire: function () { } })
+const apiKeyAuth = {
+  apiKey: 'ak_live_lkjsdhfkjsdhfjkd'
+}
+
+const encryptionKeyAuth = {
+  encryptionKey: 'ek_live_lkjsdhfkjsdhfjkd'
+}
+
+pagarme.connect(emailPasswordAuth)
+	.then(client => client.transaction.get(123))
+	.then(console.log.bind(console))
+
+pagarme.connect(apiKeyAuth)
+	.then(client => client.transaction.get(123))
+	.then(console.log.bind(console))
+
+pagarme.connect(encryptionKeyAuth)
 	.then(client => client.transaction.get(123))
 	.then(console.log.bind(console))
 
