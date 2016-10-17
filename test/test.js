b8531c048f39e0c6ad71c3397315de49ee49d9ed
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-10-17 14:44:00 -0200

    Refactored test to use a temporary company

--- test/test.js
+++ test/test.js
@@ -1,30 +1,40 @@
-import pagarme from '../dist/pagarme.js';
+const pagarme = require('../dist/pagarme.js').default
+const fetch = require('node-fetch')
 
-const emailPasswordAuth = {
-  email: 'jonathan@pagar.me',
-  password: 'asdas123',
-  onExpire: function () {
-    console.log('session expired')
-  }
-}
+let tempCompany = {}
 
-const apiKeyAuth = {
-  apiKey: 'ak_live_lkjsdhfkjsdhfjkd'
-}
+fetch('https://api.pagar.me/1/companies/temporary', { method: 'POST' })
+  .then(res => res.json())
+  .then((data) => {
+    tempCompany = data
+    console.log(tempCompany)
 
-const encryptionKeyAuth = {
-  encryptionKey: 'ek_live_lkjsdhfkjsdhfjkd'
-}
+    const emailPasswordAuth = {
+      email: tempCompany.email,
+      password: tempCompany.password,
+      onExpire () {
+        console.log('session expired')
+      }
+    }
 
-pagarme.client.connect(emailPasswordAuth)
-  .then(client => client.transaction.get(123))
-  .then(console.log.bind(console))
-  .catch(err => console.dir(err.response))
+    const apiKeyAuth = {
+      apiKey: tempCompany.api_key
+    }
 
-pagarme.client.connect(apiKeyAuth)
-  .then(client => client.transaction.get(123))
-  .then(console.dir.bind(console))
+    const encryptionKeyAuth = {
+      encryptionKey: tempCompany.encryption_key
+    }
 
-pagarme.client.connect(encryptionKeyAuth)
-  .then(client => client.transaction.get(123))
-  .then(console.dir.bind(console))
+    pagarme.client.connect(emailPasswordAuth)
+      .then(client => client.transaction.get(123))
+      .then(console.log.bind(console))
+      .catch(err => console.dir(err.response))
+
+    pagarme.client.connect(apiKeyAuth)
+      .then(client => client.transaction.get(123))
+      .then(console.dir.bind(console))
+
+    pagarme.client.connect(encryptionKeyAuth)
+      .then(client => client.transaction.get(123))
+      .then(console.dir.bind(console))
+  })
