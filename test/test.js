74213fddc05d5b2f3d49d2211900c5e5d1aa0905
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-10-17 15:44:41 -0200

    Updated test to match the args for transaction.get

--- test/test.js
+++ test/test.js
@@ -26,15 +26,17 @@ fetch('https://api.pagar.me/1/companies/temporary', { method: 'POST' })
     }
 
     pagarme.client.connect(emailPasswordAuth)
-      .then(client => client.transaction.get(123))
+      .then(client => client.transaction.get({}, 123))
       .then(console.log.bind(console))
       .catch(err => console.dir(err.response))
 
     pagarme.client.connect(apiKeyAuth)
-      .then(client => client.transaction.get(123))
+      .then(client => client.transaction.get({}, 123))
       .then(console.dir.bind(console))
+      .catch(err => console.dir(err.response))
 
     pagarme.client.connect(encryptionKeyAuth)
-      .then(client => client.transaction.get(123))
+      .then(client => client.transaction.get({}, 123))
       .then(console.dir.bind(console))
+      .catch(err => console.dir(err.response))
   })
