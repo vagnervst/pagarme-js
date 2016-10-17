0f08bb757f06b5d279e0cc9269164c318773d9b0
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-10-17 15:43:59 -0200

    Updated transactions to add request for `get`

--- src/lib/transaction/index.js
+++ src/lib/transaction/index.js
@@ -1,4 +1,7 @@
-const get = (opts, id) => Promise.resolve({ id, opts })
+import routes from '../routes'
+import request from '../request'
+
+const get = (opts, id) => request.get(opts, routes.transaction.details(id), {})
 
 export default {
   get
