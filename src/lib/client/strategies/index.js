e73e08ca2bfa2897dbaf475234e9492426ee9ddb
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-10-18 10:20:20 -0200

    Refactor strategy finder

--- src/lib/client/strategies/index.js
+++ src/lib/client/strategies/index.js
@@ -1,14 +1,20 @@
+import { both, has, cond } from 'ramda'
 import encryption from './encryption'
 import login from './login'
 import api from './api'
 
-function find (options) {
-  let strategy = false
+const strategyBuilder = cond([
+  [both(has('email'), has('password')), login.build],
+  [has('apiKey'), api.build],
+  [has('encryptionKey'), encryption.build]
+])
 
-  if (options.email && options.password) strategy = login.build
-  if (options.apiKey) strategy = api.build
-  if (options.encryptionKey) strategy = encryption.build
-  if (strategy) return Promise.resolve(strategy(options))
+function find (options) {
+  const strategy = strategyBuilder(options)
+
+  if (strategy) {
+    return Promise.resolve(strategy)
+  }
 
   return Promise.reject(new Error('You must supply a valid authentication object'))
 }
