eabfa4e3d63f6184f1c93f0f554552bc4c87c9b0
Derek Stavis <dekestavis@gmail.com>
2016-12-29 18:16:45 -0200

    Remove unused endpoints on session route

--- src/lib/session/index.js
+++ src/lib/session/index.js
@@ -4,12 +4,7 @@ import request from '../request'
 const create = (opts, email, password) =>
   request.post(opts, routes.session, { email, password })
 
-const verify = Promise.resolve(1)
-const destroy = Promise.resolve(1)
-
 export default {
   create,
-  verify,
-  destroy,
 }
 
