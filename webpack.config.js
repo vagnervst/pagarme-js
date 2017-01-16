7c500112f51f412ef80dec605339f869910d46a3
Derek Stavis <dekestavis@gmail.com>
2017-01-16 03:12:51 -0200

    webpack: Build source map file

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const path = require('path')
 module.exports = {
   context: path.join(__dirname, './src'),
   entry: './index.js',
+  devtool: 'source-map',
   target: 'node',
   output: {
     path: path.join(__dirname, './dist'),
     libraryTarget: 'commonjs2',
     filename: 'pagarme.js',
+    sourceMapFilename: 'pagarme.js.map',
   },
   module: {
     loaders: [
