064846623a2593418126c497dc07bd59d7ed2962
Felquis Gimenes <frgformenton@gmail.com>
2017-01-27 04:56:32 -0200

    fix ESLint no-tabs rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
         "no-useless-escape": "warn",
         "no-undef": "warn",
         "no-array-constructor": "warn",
-        "no-tabs": "warn",
         "no-unused-vars ": "warn",
         "brace-style": "warn"
     }
