cfea930e4e2d29759c4febeb16a4ff463537dd3d
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2017-02-06 11:14:41 -0200

    session: added destroy function

--- src/lib/client/resources.js
+++ src/lib/client/resources.js
@@ -2,10 +2,12 @@ import transaction from '../transaction'
 import search from '../search'
 import user from '../user'
 import company from '../company'
+import session from '../session'
 
 export default {
   company,
   search,
+  session,
   transaction,
   user,
 }
