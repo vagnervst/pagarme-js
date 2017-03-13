862e986d55e1d9546ba9f2be04981d5fb4ae388c
Marco Worms <marcogworms@gmail.com>
2017-03-13 14:25:04 -0300

    tests: add longer timeout interval so travis wont fail over nothing

--- test/runTest.js
+++ test/runTest.js
@@ -1,10 +1,12 @@
 import {
   merge,
-  toPairs,
 } from 'ramda'
+
 import pagarme from '../dist/pagarme'
 
+
 export default function (test) {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
   const opts = {
     options: { baseURL: 'http://127.0.0.1:8080' },
   }
